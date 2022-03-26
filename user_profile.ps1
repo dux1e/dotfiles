96b891cb7b1028dc5fa7941a87dfec971bd154f0
Joakim Christensen <39315944+dux1e@users.noreply.github.com>
2022-03-26 10:50:56 +0100

    Add files via upload

---
+++ user_profile.ps1
@@ -0,0 +1,18 @@
+# set PowerShell to UTF-8
+[console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
+
+# Promt
+Import-Module posh-git
+Import-Module oh-my-posh
+$omp_config = Join-Path $PSScriptRoot ".\takuya.omp.json"
+oh-my-posh --init --shell pwsh --config $omp_config | Invoke-Expression
+
+Import-Module -Name Terminal-Icons
+
+#PSReadLine
+Set-PSReadLineOption -PredictionSource History
+
+# Alias
+Set-Alias ll ls
+Set-Alias grep findstr
+Set-Alias g git
