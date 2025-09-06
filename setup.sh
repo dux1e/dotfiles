9bb8d510f12e9acf9c4e40f519ab2a52907fe053
dux1e <joakimch@proton.me>
2025-09-07 00:34:05 +0200

    fixed script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,13 @@
+function install_oh_my_posh() {
+  curl -s https://ohmyposh.dev/install.sh | bash -s
+  mkdir -p ~/.config/oh-my-posh
+  cp .config/oh-my-posh/* ~/.config/oh-my-posh/
+}
+
+function setup_bashrc() {
+  cp .config/.bashrc ~/.bashrc
+}
+
 DISTRO=$(grep -oP '(?<=^NAME=").*(?=")' /etc/os-release | tr '[:upper:]' '[:lower:]')
 
 mkdir -p ~/.local/bin
@@ -5,7 +15,7 @@ mkdir -p ~/.local/bin
 case "$DISTRO" in
 "arch linux")
   sudo pacman -Syu
-  sudo pacman -S base-devel go jq yq fzf ripgrep github-cli
+  sudo pacman -S base-devel go jq yq fzf ripgrep github-cli unzip
   setup_bashrc
   install_oh_my_posh
   ;;
@@ -20,13 +30,3 @@ case "$DISTRO" in
   exit 0
   ;;
 esac
-
-function install_oh_my_posh() {
-  curl -s https://ohmyposh.dev/install.sh | bash -s
-  mkdir -p ~/.config/oh-my-posh
-  mv .config/oh-my-posh/* ~/.config/oh-my-posh/
-}
-
-function setup_bashrc() {
-  mv .config/.bashrc ~/.bashrc
-}
